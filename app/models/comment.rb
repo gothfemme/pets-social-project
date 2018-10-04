fc7fa8d69d4f5e5c7aa667ff6bd4d61132856371
Kat Michaela <k@gothfem.me>
2018-10-04 14:42:25 -0400

    idk

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,8 @@ class Comment < ApplicationRecord
   belongs_to :post
 
   validates :content, presence: true
+
+  def create_date
+    self.created_at.localtime.strftime("%I:%M%P %B %d %Y")
+  end
 end
